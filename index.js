741b480ded1b8b4a476992f9b84cf28969185eae
farhaanxxiviv <70432375+farhaanxxiviv@users.noreply.github.com>
2021-06-09 15:32:02 +0530

    Add CORS policy

--- index.js
+++ index.js
@@ -17,11 +17,17 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
     });
 
 
-    app.use(express.json());
+    app.use(function (req, res, next) {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+        res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+        next();
+    });
+
 
 
     app.get('/', (req,res) =>{
-        res.send('Hello World')
+        res.send('Hello World Farhaan')
     })
 
 

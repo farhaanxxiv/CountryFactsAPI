a0c70d48309313f3c2f19b93d942ec98dbd4fb68
farhaanxxiviv <70432375+farhaanxxiviv@users.noreply.github.com>
2021-06-14 14:58:35 +0530

    Added Simple Home Page
    index page with ejs and some simple instructions

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const mongoose = require('mongoose');
 const Country = require('./countrySchema')
 
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }))
 
 
 const uri = "mongodb+srv://farhaanxxiv:farhaan24@cluster0.tsjcz.mongodb.net/Countries?retryWrites=true&w=majority";
@@ -27,29 +29,30 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
 
 
     app.get('/', (req,res) =>{
-        res.send('Hello World Farhaan')
+      //  res.send('Random Country Facts API---https://countryfacts-api.herokuapp.com/{countryName}')
+      res.render('index')
     })
 
 
-    app.get('/save', async (req,res)=>{
+    // app.get('/save', async (req,res)=>{
 
-        try {
-            let post = new Country({
-                name: 'ind',
-                facts: ["bad", "good"] 
-            });
+    //     try {
+    //         let post = new Country({
+    //             name: 'ind',
+    //             facts: ["bad", "good"] 
+    //         });
         
-            post = await post.save()
-            console.log(post);
+    //         post = await post.save()
+    //         console.log(post);
           
-        } catch (e) {
+    //     } catch (e) {
         
         
-            console.error(e);
+    //         console.error(e);
         
-        }
+    //     }
         
-    }) 
+    // }) 
 
 
 app.get('/:id', async (req,res) => {
@@ -58,13 +61,14 @@ app.get('/:id', async (req,res) => {
         let post = await Country.find({name:req.params.id})
         
         let rand = Math.floor(Math.random() * 4);
-        console.log(rand)
+        //console.log(rand)
         res.send(post[0].facts[rand])
 
 
         }catch(e){
     
             console.log(e);
+                res.send('Incorrect Country or Country Not Available')
         }
 
         
